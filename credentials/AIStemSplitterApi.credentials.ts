4e6100aa97cd544a9d4660d144fcfb3b6a330a7c
codeugar <161671203+codeugar@users.noreply.github.com>
2026-05-10 02:10:21 +0800

    fix: inline n8n credential test source

--- credentials/AIStemSplitterApi.credentials.ts
+++ credentials/AIStemSplitterApi.credentials.ts
@@ -1,1 +1,40 @@
-export { AIStemSplitterApi } from './AIStemSplitterApi.credentials.js';
+export class AIStemSplitterApi {
+  name = 'aiStemSplitterApi';
+  displayName = 'AIStemSplitter API';
+  documentationUrl = 'https://aistemsplitter.org/developers/api';
+
+  authenticate = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: '=Bearer {{$credentials.apiKey}}',
+      },
+    },
+  };
+
+  test = {
+    request: {
+      baseURL: '={{$credentials.apiBaseUrl}}',
+      url: '/credits',
+      method: 'GET',
+    },
+  };
+
+  properties = [
+    {
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string',
+      typeOptions: { password: true },
+      default: '',
+      required: true,
+    },
+    {
+      displayName: 'API Base URL',
+      name: 'apiBaseUrl',
+      type: 'string',
+      default: 'https://api.aistemsplitter.org/v1',
+      required: true,
+    },
+  ];
+}
